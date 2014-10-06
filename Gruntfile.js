477b4de40b2f699b1031efbed0f35f931ba7697b
unknown <umilanovich@umilanovich-MI-1069.ELTEGRA.BY>
2014-10-06 11:31:19 +0300

    Change Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,23 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
-		uglify: {
-			target: {
-				files: {
-					'dest/output.min.js': ['src/input1.js', 'src/input2.js']
-				}
-			}
-  		},
-		//
-		concat: {
-		    extras: {
-		      src: ['src/main.js', 'src/extras.js'],
-		      dest: 'dist/with_extras.js',
-		    },
-  		},
   		// running `grunt less` will compile once
 		less: {
 			development: {
 				options: {
-					paths: ["../scr/css/result"],
+					paths: ["./scr/css"],
 					yuicompress: true
 				},
 				files: {
-					"../src/css/result/style.css": "../src/css/source/style.less"
+					"./src/css/style.css": "./src/css/style.less"
 				}
 			}
 		},
 		// running `grunt watch` will watch for changes
 		watch: {
-			files: ".././src/css/source/*.less",
+			files: "./src/css/*.less",
 			tasks: ["less"]
 		}
 	});
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 };
