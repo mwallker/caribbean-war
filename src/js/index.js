be2814a658b796d2ef35942a4680cf371404b5f4
unknown <umilanovich@umilanovich-MI-1069.ELTEGRA.BY>
2014-10-07 11:42:13 +0300

    Add event-based architecture

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,18 @@
+// +--------------------+
+// | 	USER DATAS 		|
+// +--------------------+
 var userInfo = {
 	id:false,
 	name:false,
-	cash:false
+	cash:false,
+	ship:{
+
+	}
 }
 
+// +----------------------+
+// | INPUT EVENTS HANDLE: |
+// +----------------------+
 var conn = new Connector();
 
 function auth(credits){
@@ -18,19 +27,31 @@ function auth(credits){
 	conn.wsOpen(request);
 }
 
+
+// +----------------------------------+
+// | INCOMMING MESSAGES EVENT HANDLE: |
+// +----------------------------------+
 function handle(data){
 	console.log(data);
-	if(data.action == "auth"){ 
-		if(data.details!="{}"){
-			userInfo.id = data.details["ID"];
-    		userInfo.name = data.details["Email"];
-    		userInfo.cash = data.details["Cash"];
-    		console.log(userInfo);
-    	}
-    	else{
-    		conn.wsClose();
-    	}
-  	}
-}     
 
+	if(data.action){
+		$("body, html").trigger({
+			type:data.action,
+			details:data.details
+		});
+	}	
+}  
+
+$("body, html").on("auth", function(event){
+	console.log(event.details);
+	if(data.details!="{}"){
+		userInfo.id = data.details["ID"];
+    	userInfo.name = data.details["Email"];
+    	userInfo.cash = data.details["Cash"];
+    	console.log(userInfo);
+    }
+    else{
+    	conn.wsClose();
+    }
+});
 
