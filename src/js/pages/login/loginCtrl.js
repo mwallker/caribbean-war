e14717e79d7cb484647df7c871912487fee586d5
unknown <umilanovich@umilanovich-MI-1069.ELTEGRA.BY>
2014-10-09 17:37:47 +0300

    Fix authorization#3

--- src/js/pages/login/loginCtrl.js
+++ src/js/pages/login/loginCtrl.js
@@ -13,16 +13,17 @@ angular.module('caribbean-war').controller('loginCtrl', function ($scope, $state
 		if(localStorage.confirm){
 			localStorage.email = $scope.email;
 			var t = angular.toJson({
-				login: $scope.email,
-				password: new jsSHA(($scope.password).toString(), 'TEXT').getHash('SHA-256', 'HEX')
-			})
+				action:"auth",
+				details:{
+					login: $scope.email,
+					password: new jsSHA(($scope.password).toString(), 'TEXT').getHash('SHA-256', 'HEX')
+				}
+			});
 			console.log(t);
 			connection.open(t);
 		}
 		else{
 
 		}
-		console.log(localStorage.email);
 	}
-	console.log("LoL");
 });
