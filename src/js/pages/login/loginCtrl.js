815761434adccdc53349512cfb1fbcd784056968
VladimirMilanovich <vladimir.milanovich@gmail.com>
2014-10-12 23:01:11 +0300

    Fix some stuff

--- src/js/pages/login/loginCtrl.js
+++ src/js/pages/login/loginCtrl.js
@@ -1,34 +1,23 @@
 angular.module('caribbean-war').controller('loginCtrl', function ($scope, $state, connection) {
 
 	$scope.email = localStorage.email||"";
-	$scope.confirm = localStorage.confirm;
 
-	console.log(localStorage.confirm);
-
-	$scope.remember = function() {
-
-		localStorage.confirm = $scope.confirm;
-
-		console.log(localStorage.confirm);
-	}
+	console.log(localStorage.remember);
 
 	$scope.connect = function(){
-		if(localStorage.confirm){
+		localStorage.email = $scope.email || "";
 
-			localStorage.email = $scope.email;
-
-			var credits = {
-				login: $scope.email,
-				password: new jsSHA(($scope.password).toString(), 'TEXT').getHash('SHA-256', 'HEX')
-			}
-
-			console.log(credits);
-
-			connection.open(credits).then(
-				function(){
-					connection.send("auth", credits);
-				}
-			);	
+		var credits = {
+			login: $scope.email,
+			password: new jsSHA(($scope.password).toString(), 'TEXT').getHash('SHA-256', 'HEX')
 		}
+
+		console.log(credits);
+
+		connection.open(credits).then(
+			function(){
+				connection.send("auth", credits);
+			}
+		);
 	}
 });
