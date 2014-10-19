e0ff96ba7774af43b4bf7bebdfdf2ab21e39f077
Aliaksandr Zasim <al.zasim@gmail.com>
2014-10-19 09:58:22 +0300

    Update gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,58 @@
 module.exports = function(grunt) {
+
+	require('load-grunt-tasks')(grunt);
+
 	grunt.initConfig({
-  		// running `grunt less` will compile once
 		less: {
 			development: {
-				options: {
-					paths: ["./scr/css"],
-					yuicompress: true
-				},
 				files: {
-					"./src/css/style.css": "./src/css/*.less"
+					"src/css/style.css": "src/css/*.less"
 				}
 			}
 		},
-		// running `grunt watch` will watch for changes
 		watch: {
-			files: "./src/css/*.less",
-			tasks: ["less"]
+			options: {
+				atBegin: true
+			},
+			less: {
+				files: "src/css/*.less",
+				tasks: ["less"]
+			},
+			js: {
+				files: ["src/**/*.js", "!src/js/libs/**/*"],
+				tasks: ["newer:jshint"]
+			}
+		},
+		jshint: {
+			all: {
+				src: ["src/**/*.js", "!src/js/libs/**/*"]
+			}
+		},
+		'http-server': {
+			dev: {
+				root: "src",
+				port: 9000,
+				host: "127.0.0.1",
+				runInBackground: true
+			}
+		},
+		nodewebkit: {
+			options: {
+				platforms: ['win'],
+				buildDir: './bin'
+			},
+			src: ['src/**/*']
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default', [
+		'http-server',
+		'watch'
+	]);
+
+	grunt.registerTask('build', [
+		'jshint',
+		'less',
+		'nodewebkit'
+	]);
 };
