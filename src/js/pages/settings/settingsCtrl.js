cd7209d8c33f09cb008fccc3f6d8049193860615
Uladzimir Milanovich <vladimir.milanovich@gmail.com>
2015-02-16 09:50:26 +0300

    Improve audio service

--- src/js/pages/settings/settingsCtrl.js
+++ src/js/pages/settings/settingsCtrl.js
@@ -1,42 +1,27 @@
 angular.module('caribbean-war')
 	.controller('settingsCtrl', ['$scope', '$rootScope', '$translate', 'audioControl', 'appConfig',
-		function($scope, $rootScope, $translate, audioControl, appConfig){
+		function ($scope, $rootScope, $translate, audioControl, appConfig) {
+			$scope.languages = appConfig.languages;
 
-            $scope.musicVolume = 100;
-            $scope.languages = appConfig.languages;
-            $scope.locale= localStorage.locale || appConfig.languages[0].code;
+			$scope.musicVolume = localStorage.musicVolume || 100;
+			$scope.effectsVolume = localStorage.effectsVolume || 100;
+			$scope.locale = localStorage.locale || appConfig.languages[0].code;
 
-            //locale
-            $scope.$watch('locale', function(newVal, oldVal){
-                if(newVal != oldVal){
-                   $translate.use(newVal);
-                }
-            });
+			//locale
+			$scope.$watch('locale', function (newVal, oldVal) {
+				if (newVal != oldVal) {
+					$translate.use(newVal);
+				}
+			});
 
-            $scope.saveConfigurations = function () {
-                localStorage.locale = $scope.locale;
-            };
+			$scope.saveConfigurations = function () {
+				localStorage.locale = $scope.locale;
+				localStorage.musicVolume = $scope.musicVolume;
+				localStorage.effectsVolume = $scope.effectsVolume;
+			};
 
-            $scope.changeVolume = function(target){
-                audioControl.changeVolume($scope.musicVolume/100, target);
-            };
+			$scope.changeVolume = function (target) {
+				audioControl.changeVolume($scope.musicVolume / 100, target);
+			};
 
-        }]);
-/*
-(function () {
-    var root = $(document.getElementsByTagName('body'));
-    var watchers = [];
-    var f = function (element) {
-        if (element.data().hasOwnProperty('$scope')) {
-            angular.forEach(element.data().$scope.$$watchers, function (watcher) {
-                watchers.push(watcher);
-            });
-        }
-        angular.forEach(element.children(), function (childElement) {
-            f($(childElement));
-        });
-    };
-    f(root);
-    console.log(watchers.length);
-})();
-*/
+		}]);
--- src/js/sound/audioService.js
+++ src/js/sound/audioService.js
@@ -1,38 +1,44 @@
 caribbeanWarApp.service('audioControl', function ($q) {
 	window.AudioContext = window.AudioContext || window.webkitAudioContext;
 	var context = new AudioContext();
-    var gainNode = context.createGain();
-	var audioBuffer, source, destination;
+	var gainNodes = {
+		music: context.createGain(),
+		effects: context.createGain()
+	}
+	var destination = context.destination;
+	var sources = [];
+
+	gainNodes.music.connect(destination);
+	gainNodes.effects.connect(destination);
+
 	var audioApi = {};
 
-	// функция для подгрузки файла в буфер
-	audioApi.loadSoundFile = function(url) {
-        var deffered = $q.defer();
+	audioApi.loadSoundFile = function (url, type) {
+		var deffered = $q.defer();
 		var request = new XMLHttpRequest();
 		request.open('GET', url, true);
 		request.responseType = 'arraybuffer';
 
 		// Decode asynchronously
-		request.onload = function() {
-			context.decodeAudioData(request.response, function(buffer) {
-				audioBuffer = buffer;
-				source = context.createBufferSource();
-				source.buffer = audioBuffer;
-				destination = context.destination;
-                source.connect(gainNode);
-                gainNode.connect(context.destination);
-				source.start(0);
-                deffered.resolve();
+		request.onload = function () {
+			context.decodeAudioData(request.response, function (buffer) {
+				var index = sources.push(context.createBufferSource());
+				sources[index] = context.createBufferSource();
+				sources[index].buffer = buffer;
+				sources[index].connect(gainNodes[type || 'effects']);
+				sources[index].start(0);
+				deffered.resolve();
 			});
 		};
 		request.send();
-        return deffered.promise;
+		return deffered.promise;
 	};
 
-    audioApi.changeVolume = function(value){
-        if(gainNode) gainNode.gain.value = value;
-        console.log(value);
-    };
+	audioApi.changeVolume = function (value, target) {
+		if (target && gainNodes[target]) {
+			gainNodes[target].gain.value = value;
+		}
+	};
 
 	return audioApi;
 });
