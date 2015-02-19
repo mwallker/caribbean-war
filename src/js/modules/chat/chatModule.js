e0e7426592b8917c93267bb5bbd161aa74a15f51
Uladzimir Milanovich <vladimir.milanovich@gmail.com>
2015-02-19 14:05:27 +0300

    Chat: fix+improve

--- src/js/modules/chat/chatModule.js
+++ src/js/modules/chat/chatModule.js
@@ -1,41 +1,46 @@
 angular.module('caribbean-war')
-    .directive('chat', ['userStorage', 'connection', function (userStorage, connection) {
-        return { 
-            templateUrl: 'js/modules/chat/chat-template.html',
-            restrict: 'E',
-            scope:{},
-            controller: function ($scope, $rootScope) {
-                $scope.chatHistory = [];
+	.directive('chat', ['userStorage', 'connection', function (userStorage, connection) {
+			return {
+				templateUrl: 'js/modules/chat/chat-template.html',
+				restrict: 'E',
+				scope: {},
+				controller: function ($scope, $rootScope) {
+					$scope.chatHistory = [];
+					$scope.battleLog = [];
+					$scope.chatBuffer = 4;
 
-                $scope.clearChatHistory = function(){
-                    $scope.chatHistory = [];
-                };
+					$scope.clearChatHistory = function () {
+						$scope.chatHistory = [];
+					};
 
-                $scope.recieveChatMessage = function(event, data){
-                    if($scope.chatHistory.length >= 50) $scope.chatHistory.shift();
+					$scope.recieveChatMessage = function (event, data) {
+						if ($scope.chatHistory.length >= $scope.chatBuffer) $scope.chatHistory.shift();
 
-                    $scope.chatHistory.push({
-                        from:data.sender,
-                        message:data.message
-                    });
+						$scope.chatHistory.push({
+							from: data.sender,
+							message: data.message,
+							timestamp: data.timestamp,
+						});
 
-                    $scope.$apply();
-                };
+						$scope.$apply();
+					};
 
-                $scope.sendChatMessage = function(){
-                    var text = $scope.message;
+					$scope.sendChatMessage = function () {
+						var text = $scope.message;
 
-                    if(text){
-                        connection.send("chat", {
-                            sender: userStorage.get().Nick,
-                            message: text
-                        });
-                        $scope.message = '';
-                    }
-                };
+						if (text) {
+							connection.send("chat", {
+								sender: userStorage.get().nick,
+								senderId: userStorage.get().id,
+								timestamp: Date.now(),
+								message: text
+							});
+							$scope.message = '';
+						}
+					};
 
-                $rootScope.$on("chat", $scope.recieveChatMessage);
-            }           
-        };
+					$rootScope.$on("chat", $scope.recieveChatMessage);
+				}
+			};
     }
 ]);
