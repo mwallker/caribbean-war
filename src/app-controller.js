db91945af45a500c3dec2bb4db647549e246f129
Uladzimir Milanovich <vladimir.milanovich@gmail.com>
2015-03-12 12:00:55 +0300

    Move events to separate file

--- src/app-controller.js
+++ src/app-controller.js
@@ -1,12 +1,10 @@
 angular.module('caribbean-war')
-	.controller('appCtrl', ['$scope', '$rootScope', '$q', '$state', '$timeout', 'audioControl', 'connection', 'renderHandler', 'userStorage',
-		function ($scope, $rootScope, $q, $state, $timeout, audioControl, connection, renderHandler, userStorage) {
+	.controller('appCtrl', ['$scope', '$rootScope', '$q', '$state', 'renderHandler', 'audioControl',
+		function ($scope, $rootScope, $q, $state, renderHandler, audioControl) {
 
 			$scope.appLoading = true;
-			$scope.errorShown = false;
-
 			$scope.manageTasks = function (tasks) {
-				$scope.appLoading = true;
+				//$scope.appLoading = true;
 				$q.all(tasks).then(function () {
 					$scope.appLoading = false;
 				}, function () {
@@ -16,71 +14,9 @@ angular.module('caribbean-war')
 
 			$scope.manageTasks([ /*audioControl.loadSoundFile('theme', 'music'), audioControl.loadSoundFile('ocean', 'effects')*/ ]);
 
-			$scope.errorHide = function () {
-				$scope.errorType = '';
-				$scope.errorShown = false;
-			};
-
-			$rootScope.$on('error', function (event, type) {
-				$rootScope.loading = false;
-				if (!$scope.errorShown && type) {
-					$scope.errorType = type;
-					$scope.errorShown = true;
-					$timeout($scope.errorHide, 5000);
-				}
-			});
-
-			$rootScope.$on('open', function (event, credits) {
-				$rootScope.loading = true;
-				connection.open().then(
-					function () {
-						connection.send('auth', credits);
-					},
-					function () {
-						$rootScope.$emit('error', 'ERRORS_CONN');
-					});
-			});
-
-			$rootScope.$on('send', function (event, data) {
-				try {
-					$rootScope.loading = true;
-					connection.send(data.action, data.details);
-				} catch (e) {
-					$rootScope.$emit('error', 'ERRORS_SEND_FAIL');
-				}
-			});
-
-			$rootScope.$on('close', function (event, wasClean) {
-				connection.close();
-				userStorage.reset();
-				$state.go('login');
-				if (!wasClean) $rootScope.$emit('error', 'ERRORS_ROUT');
-			});
-
-			$rootScope.$on('exit', function (event) {
-
-			});
-
-			$rootScope.$on('fuckup', function () {
-				$rootScope.$emit('error', 'ERRORS_FUCKUP');
-			});
-
-			$rootScope.$on('exitWorld', function (event) {
-				connection.send('exitWorld', {});
-				$state.go('harbor');
-			});
-
-			$rootScope.$on('enterWorld', function (event, details) {
-				if (details.success === true) {
-					userStorage.setNeighbors(details);
-					userStorage.setShip(details);
-					$state.go('world');
-				}
-			});
-
 			$rootScope.$on('$stateChangeStart',
 				function (event, toState) {
-					$scope.errorShown = false;
+					$rootScope.$emit('error', false);
 					renderHandler.dispose();
 				});
 
@@ -93,8 +29,7 @@ angular.module('caribbean-war')
 
 			$rootScope.$on('$stateChangeError',
 				function (event, toState) {
-					$scope.errorType = 'ERROR_ROUT';
-					$scope.errorShown = true;
+					$rootScope.$emit('error', 'ERROR_ROUT');
 				});
 		}
 	]);
---
+++ src/events-controller.js
@@ -0,0 +1,76 @@
+angular.module('caribbean-war')
+	.controller('eventsCtrl', ['$scope', '$rootScope', '$state', '$timeout', 'connection', 'userStorage',
+		function ($scope, $rootScope, $state, $timeout, connection, userStorage) {
+
+			$scope.errorShown = false;
+
+			//CONNECTION EVENTS
+			$rootScope.$on('open', function (event, credits) {
+				$rootScope.loading = true;
+				connection.open().then(
+					function () {
+						connection.send('auth', credits);
+					},
+					function () {
+						$rootScope.$emit('error', 'ERRORS_CONN');
+					});
+			});
+
+			$rootScope.$on('send', function (event, data) {
+				try {
+					$rootScope.loading = true;
+					connection.send(data.action, data.details);
+				} catch (e) {
+					$rootScope.$emit('error', 'ERRORS_SEND_FAIL');
+				}
+			});
+
+			$rootScope.$on('close', function (event, wasClean) {
+				connection.close();
+				userStorage.reset();
+				$state.go('login');
+				if (!wasClean) $rootScope.$emit('error', 'ERRORS_ROUT');
+			});
+
+
+			//INFORMATION EVENTS
+			$rootScope.$on('wait', function (event, data) {
+
+			});
+
+			$rootScope.$on('error', $scope.errorHandler);
+			$scope.errorHandler = function (event, type) {
+				if (type) {
+					if ($scope.errorShown) return;
+					$scope.errorType = type;
+					$scope.errorShown = true;
+					$timeout(function () {
+						$scope.errorType = '';
+						$scope.errorShown = false;
+					}, 5000);
+				} else {
+					$scope.errorType = '';
+					$scope.errorShown = false;
+				}
+			}
+
+			//TODO: bad event, remove it later on
+			$rootScope.$on('fuckup', function () {
+				$rootScope.$emit('error', 'ERRORS_FUCKUP');
+			});
+
+			//WORLD GLOBAL EVENTS
+			$rootScope.$on('enterWorld', function (event, details) {
+				if (details.success === true) {
+					userStorage.setNeighbors(details);
+					userStorage.setShip(details);
+					$state.go('world');
+				}
+			});
+
+			$rootScope.$on('exitWorld', function (event) {
+				connection.send('exitWorld', {});
+				$state.go('harbor');
+			});
+
+		}]);
